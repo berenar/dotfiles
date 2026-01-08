ac9f0b194e7054b8b258ad7804f94c1466f359e2
Bernat Pericàs <bernatpericasserra97@gmail.com>
2026-01-08 18:01:00 +0100

    fix: rewrite rename_file with proper async handling

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -1,37 +1,32 @@
 -- https://github.com/neovim/neovim/issues/20784
 local function rename_file()
-	local source_file, target_file
+  local source_file = vim.api.nvim_buf_get_name(0)
 
-	vim.ui.input({
-		prompt = "Source : ",
-		completion = "file",
-		default = vim.api.nvim_buf_get_name(0),
-	}, function(input)
-		source_file = input
-	end)
-	vim.ui.input({
-		prompt = "Target : ",
-		completion = "file",
-		default = source_file,
-	}, function(input)
-		target_file = input
-	end)
+  vim.ui.input({
+    prompt = "Source: ",
+    completion = "file",
+    default = source_file,
+  }, function(source)
+    if not source or source == "" then return end
 
-	local params = {
-		command = "_typescript.applyRenameFile",
-		arguments = {
-			{
-				sourceUri = source_file,
-				targetUri = target_file,
-			},
-		},
-		title = "",
-	}
+    vim.ui.input({
+      prompt = "Target: ",
+      completion = "file",
+      default = source,
+    }, function(target)
+      if not target or target == "" then return end
 
-	vim.lsp.util.rename(source_file, target_file)
-	vim.lsp.buf.execute_command(params)
-	-- TODO: write changed buffer
+      vim.lsp.util.rename(source, target)
+      vim.lsp.buf.execute_command({
+        command = "_typescript.applyRenameFile",
+        arguments = {{ sourceUri = source, targetUri = target }},
+      })
+      vim.cmd("edit " .. target)
+    end)
+  end)
 end
+
+vim.api.nvim_create_user_command("RenameFile", rename_file, {})
 
 return {
 	{
