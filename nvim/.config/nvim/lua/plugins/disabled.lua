ee1da20940f7ea4b545083206dcb68776f0cc414
Bernat Pericàs <bernatpericasserra97@gmail.com>
2026-01-08 18:02:02 +0100

    refactor: merge miniintent.lua into disabled.lua

--- nvim/.config/nvim/lua/plugins/disabled.lua
+++ nvim/.config/nvim/lua/plugins/disabled.lua
@@ -2,8 +2,10 @@ return {
 	-- { "nvim-lualine/lualine.nvim", enabled = false },
 	{ "folke/tokyonight.nvim", enabled = false },
 	{ "echasnovski/mini.pairs", enabled = false },
-	-- { "akinsho/bufferline.nvim", enabled = false },
+	{ "nvim-mini/mini.indentscope", lazy = true, enabled = false },
+	-- { "akinsho/bufferline.nvim", enabled = false }
 	-- { "lukas-reineke/indent-blankline.nvim", enabled = false },
 	-- { "echasnovski/mini.indentscope", enabled = false },
+	{ "nvim-mini/mini.indentscope", lazy = true, enabled = false },
 	{ "folke/noice.nvim", enabled = false },
 }
