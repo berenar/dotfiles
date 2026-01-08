2064c9ed1740e3efaf167c03c37a5fbea31a36dc
Bernat Pericàs <bernatpericasserra97@gmail.com>
2026-01-08 18:41:31 +0100

    refactor: fix renamed plugin

--- nvim/.config/nvim/lua/plugins/disabled.lua
+++ nvim/.config/nvim/lua/plugins/disabled.lua
@@ -1,7 +1,7 @@
 return {
   -- { "nvim-lualine/lualine.nvim", enabled = false },
   { "folke/tokyonight.nvim", enabled = false },
-  { "echasnovski/mini.pairs", enabled = false },
+  { "nvim-mini/mini.pairs", enabled = false },
   { "nvim-mini/mini.indentscope", lazy = true, enabled = false },
   -- { "akinsho/bufferline.nvim", enabled = false }
   -- { "lukas-reineke/indent-blankline.nvim", enabled = false },
