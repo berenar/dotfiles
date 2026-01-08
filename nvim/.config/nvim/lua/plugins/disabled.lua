a75c5c2b59a9c65137636a95406f9b2ce8267cda
Bernat Pericàs <bernatpericasserra97@gmail.com>
2026-01-08 17:59:55 +0100

    fix: correct plugin organization names for mini plugins

--- nvim/.config/nvim/lua/plugins/disabled.lua
+++ nvim/.config/nvim/lua/plugins/disabled.lua
@@ -1,7 +1,7 @@
 return {
 	-- { "nvim-lualine/lualine.nvim", enabled = false },
 	{ "folke/tokyonight.nvim", enabled = false },
-	{ "nvim-mini/mini.pairs", enabled = false },
+	{ "echasnovski/mini.pairs", enabled = false },
 	-- { "akinsho/bufferline.nvim", enabled = false },
 	-- { "lukas-reineke/indent-blankline.nvim", enabled = false },
 	-- { "echasnovski/mini.indentscope", enabled = false },
