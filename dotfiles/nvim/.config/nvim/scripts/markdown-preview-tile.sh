3694acaadc9eeee7366010b2078e61a3888643a8
Bernat Pericàs <bernatpericasserra97@gmail.com>
2026-05-30 12:41:31 +0200

    put safari and kitty side by side when opening the current markdown's preview

---
+++ dotfiles/nvim/.config/nvim/scripts/markdown-preview-tile.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+url="$1"
+
+open -a Safari "$url"
+
+osascript <<'APPLESCRIPT'
+tell application "Finder"
+  set screenBounds to bounds of window of desktop
+end tell
+
+set screenWidth to item 3 of screenBounds
+set screenHeight to item 4 of screenBounds
+set menuBarHeight to 25
+set halfWidth to screenWidth / 2
+set usableHeight to screenHeight - menuBarHeight
+
+tell application "System Events"
+  repeat until (exists front window of process "Safari")
+    delay 0.02
+  end repeat
+
+  tell process "Safari"
+    set position of front window to {0, menuBarHeight}
+    set size of front window to {halfWidth, usableHeight}
+  end tell
+
+  tell process "kitty"
+    set position of front window to {halfWidth, menuBarHeight}
+    set size of front window to {halfWidth, usableHeight}
+  end tell
+end tell
+APPLESCRIPT
